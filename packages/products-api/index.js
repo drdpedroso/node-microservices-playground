24499b26ac21cddfa38e45bbf4370bb7aa6d9b67
Eduardo <drdpedroso@gmail.com>
2018-09-27 09:49:11 +0200

    Playing around

--- packages/products-api/index.js
+++ packages/products-api/index.js
@@ -3,8 +3,8 @@ import express from 'express'
 const app = express()
 
 app.get('*', (req, response) => {
-    console.log(req.url)
-    response.send('Heyi products :)')
+  console.log(req.url)
+  response.send('Hey ')
 })
 
 app.listen('3000', () => console.log('Were up and running, bro!'))
